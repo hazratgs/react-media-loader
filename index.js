1873c5c315ec09e0c0d435a06ea41fc08bc128d4
hazratgs <hazratgs@gmail.com>
2017-07-15 22:18:15 +0300

    Add propTypes params

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
 
 export default class MediaLoader extends PureComponent {
   render () {
-    
+
   }
+}
+
+MediaLoader.propTypes = {
+  loader: PropTypes.node.isRequired,
+  contents: PropTypes.array.isRequired,
+  onSuccess: PropTypes.func,
+  onError: PropTypes.func
 }
