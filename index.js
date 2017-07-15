887a749562ed5534d4a8953efb06eb0be0317294
hazratgs <hazratgs@gmail.com>
2017-07-15 23:49:35 +0300

    Прототип

--- index.js
+++ index.js
@@ -2,15 +2,45 @@ import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 
 export default class MediaLoader extends PureComponent {
-  
-  render () {
+  constructor (props) {
+    super(props)
 
+    this.state = {
+      status: false
+    }
+  }
+
+  componentDidMount () {
+    this.onLoadImages()
+  }
+
+  // Загрузка изображения
+  onLoadImages () {
+    let img = new Image()
+    img.src = this.props.media
+    img.onload = () => {
+      this.setState({status: true})
+    }
+  }
+
+  // Загрузка видео
+  onLoadVideo () {
+
+  }
+
+  render () {
+    // Отображаем Loader
+    if (!this.state.status) {
+      return this.props.loader
+    }
+    // Если контент загружен, отображаем дочерние элементы
+    return this.props.children
   }
 }
 
 MediaLoader.propTypes = {
   loader: PropTypes.node.isRequired,
-  contents: PropTypes.array.isRequired,
+  media: PropTypes.string.isRequired,
   onSuccess: PropTypes.func,
   onError: PropTypes.func
 }
